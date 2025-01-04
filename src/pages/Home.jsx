a69544e3d565d5dcebcd374a657d14c3ddab0e83
Nakul Srivastava <imnakul44@gmail.com>
2025-01-04 08:30:09 +0530

    Home Page REsponsive too completely for Tab + Laptop + Phone + Bigger Screens than latptop 1440

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
    const navigate = useNavigate()
    return (
       <>
-         <div className='w-full h-screen overflow-hidden'>
+         <div className='flex flex-col w-full h-screen overflow-hidden'>
             {/* Video Background */}
             {/* <video
                ref={videoRef}
@@ -37,66 +37,62 @@ function Home() {
             </video> */}
 
             <Navbar isLoggedIn={isLoggedIn} />
-            <div className=' flex flex-col align-center min-w-screen min-h-max m-2'>
-               <h1 className=' text-gray-800 dark:text-purple-400 mt-8 mb-3 font-extrabold text-3xl text-center'>
-                  Welcome to,
-               </h1>
-               {/* <h1
-                  className={`${
-                     theme === 'light' ? 'text-blue-800' : 'text-pink-500'
-                  }  mt-8 mb-3 font-extrabold text-3xl text-center`}
-               >
-                  Welcome to,
-               </h1> */}
-               <h1 className=' text-black dark:text-purple-300 mt-0 mb-3 font-extrabold text-6xl text-center'>
-                  The Interview Sphere
-               </h1>
-               <h4 className=' text-gray-800 dark:text-purple-400 mt-0 mb-2.5 font-extrabold text-xl text-center'>
-                  A Platform for Collaborative Inteviews.
-               </h4>
-               {/* xl:mb-[3.15rem] mb-24 */}
-               <div className='flex justify-center gap-6 mt-6'>
-                  <div className=' bg-transparent border-black flex justify-center items-center gap-3 xl:gap-5 p-2 mt-1'>
-                     {/* user  */}
-                     <div className='flex flex-col items-center border border-black p-2 m-1 w-44 xl:w-64 rounded-lg  bg-white/40'>
-                        <img
-                           src='/programmer.png'
-                           alt=''
-                           preload='auto'
-                           className='h-30 w-30 mb-4'
-                           loading='eager'
-                           fallback='/fallback.png'
-                        />
+            <div className='flex flex-1 items-center justify-center h-full'>
+               <div className=' flex flex-col align-center min-w-screen min-h-max mb-24 md:mb-20 lg:mb-12'>
+                  <h2 className=' text-gray-800 dark:text-cyan-400 mb-3 font-extrabold md:text-3xl text-xl text-center'>
+                     Welcome to,
+                  </h2>
 
-                        <button
-                           className='px-4 py-2 bg-purple-600 text-white border-2 border-purple-800 rounded-md shadow-xl hover:bg-purple-800 focus:ring-2 hover:ring-2 ring-purple-400 mb-1 hover:border-none'
-                           onClick={() => {
-                              navigate('/signin')
-                           }}
-                        >
-                           Sign In
-                        </button>
-                     </div>
+                  <h1 className=' text-black dark:text-cyan-200  mb-3 font-extrabold md:text-6xl text-5xl text-center'>
+                     The Interview Sphere
+                  </h1>
+                  <h4 className=' text-gray-800 dark:text-cyan-400  mb-3 font-extrabold md:text-xl text-lg text-center'>
+                     A Platform for Collaborative Inteviews.
+                  </h4>
+                  {/* xl:mb-[3.15rem] mb-24 */}
+                  <div className='flex justify-center gap-6'>
+                     <div className=' bg-transparent border-black flex justify-center items-center gap-3 xl:gap-5 p-2 mt-1'>
+                        {/* user  */}
+                        <div className='flex flex-col items-center border border-black p-2 m-1 w-44 xl:w-64 rounded-lg  bg-white/40'>
+                           <img
+                              src='/programmer.png'
+                              alt=''
+                              preload='auto'
+                              className='h-30 w-30 mb-4'
+                              loading='eager'
+                              fallback='/fallback.png'
+                           />
 
-                     {/* new  */}
-                     <div className='flex flex-col items-center border border-black p-2 m-1 w-44 xl:w-64 rounded-lg bg-white/40'>
-                        <img
-                           src='/boy.png'
-                           alt=''
-                           preload='auto'
-                           className='h-30 w-30 mb-4'
-                           loading='eager'
-                           fallback='/fallback.png'
-                        />
+                           <button
+                              className='px-4 py-2 bg-purple-600 text-white border-2 border-purple-800 rounded-md shadow-xl hover:bg-purple-800 focus:ring-2 hover:ring-2 ring-purple-400 mb-1 hover:border-none'
+                              onClick={() => {
+                                 navigate('/signin')
+                              }}
+                           >
+                              Sign In
+                           </button>
+                        </div>
 
-                        <button
-                           className='px-4 py-2 bg-purple-600 text-white border-2 border-purple-800 rounded-md shadow-xl hover:bg-purple-800 focus:ring-2  hover:ring-2 ring-purple-400 mb-1 hover:border-none'
-                           onClick={() => {
-                              navigate('/register')
-                           }}
-                        >
-                           Sign Up
-                        </button>
+                        {/* new  */}
+                        <div className='flex flex-col items-center border border-black p-2 m-1 w-44 xl:w-64 rounded-lg bg-white/40'>
+                           <img
+                              src='/boy.png'
+                              alt=''
+                              preload='auto'
+                              className='h-30 w-30 mb-4'
+                              loading='eager'
+                              fallback='/fallback.png'
+                           />
+
+                           <button
+                              className='px-4 py-2 bg-purple-600 text-white border-2 border-purple-800 rounded-md shadow-xl hover:bg-purple-800 focus:ring-2  hover:ring-2 ring-purple-400 mb-1 hover:border-none'
+                              onClick={() => {
+                                 navigate('/register')
+                              }}
+                           >
+                              Sign Up
+                           </button>
+                        </div>
                      </div>
                   </div>
                </div>
